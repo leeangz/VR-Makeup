643a798a4bff27c8d6a3f00b780c0e349871f94d
Seah <code.angz@gmail.com>
2023-03-16 14:47:48 +0900

    0316 색상 변경 확인 완료

--- vr2/main.py
+++ vr2/main.py
@@ -1,12 +1,11 @@
 from utils import *
 
 # Static Images
-image = cv2.imread("model.jpg", cv2.IMREAD_UNCHANGED)
+image = cv2.imread("park.jpg", cv2.IMREAD_UNCHANGED)
 
 output1 = apply_makeup(image, False, 'blush', False)
 output2 = apply_makeup(image, False, 'lips', False)
 output3 = apply_makeup(image, False, 'foundation', False)
-output4 = apply_makeup(image, False, 'eyeshadow', False)
 
 # Blend the three output images together
 alpha1 = 0.3 
@@ -18,10 +17,9 @@ blend = cv2.addWeighted(blend, 1, output3, alpha3, 0)
 
 # Display the images
 cv2.imshow("Original", image)
-cv2.imshow("Output 1", output1)
+#cv2.imshow("Output 1", output1)
 #cv2.imshow("Output 2", output2)
 #cv2.imshow("Output 3", output3)
-#cv2.imshow("Blended", blend)
-cv2.imshow("Output4", output4)
+cv2.imshow("Blended", blend)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
