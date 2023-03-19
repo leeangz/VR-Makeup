1084ec2c32c1f6c3c68874a2cfdd6c66fd059d09
Seah <code.angz@gmail.com>
2023-03-19 17:08:17 +0900

    0319 자바 플라스크 연동 완료

--- vr2/app.py
+++ vr2/app.py
@@ -1,25 +1,45 @@
+from flask import Flask, request, jsonify, Response, send_file
+from flask_cors import CORS
 import numpy as np
-from flask import Flask
-from fastapi import File, UploadFile
 from io import BytesIO
 from PIL import Image
 from utils import apply_makeup, apply_feature
-from starlette.responses import StreamingResponse
 import cv2
 import enum
 from typing import List
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    # Do something with the file
+    return 'File uploaded successfully'
 
 class FeatureChoice(str, enum.Enum):
-
     lips = 'lips'
     blush = 'blush'
     foundation = 'foundation'
 
-@app.route('/tospring')
-def spring():
-    return '스프링으로 보내'
+
+@app.route('/apply-makeup/', methods=['POST'])
+def try_makeup():
+    choice = request.form['choice']
+    filePath = request.form['filePath']
+    
+    # Read the image file from the specified file path
+    image = cv2.imread(filePath, cv2.IMREAD_UNCHANGED)
+    
+    # Apply the chosen makeup feature to the image
+    output = apply_makeup(image, False, choice, False)
+    
+    # Save the output image to a file
+    output_filepath = 'output.jpg'
+    cv2.imwrite(output_filepath, output)
+    
+    # Return the output image as a JPEG file
+    return send_file(output_filepath, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run()
--- vr2/main.py
+++ vr2/main.py
@@ -1,7 +1,7 @@
 from utils import *
 
 # Static Images
-image = cv2.imread("model.jpg", cv2.IMREAD_UNCHANGED)
+image = cv2.imread("C:\park.jpg", cv2.IMREAD_UNCHANGED)
 
 output1 = apply_makeup(image, False, 'blush', False)
 output2 = apply_makeup(image, False, 'lips', False)
@@ -16,10 +16,10 @@ blend = cv2.addWeighted(output1, alpha1, output2, alpha2, 0)
 blend = cv2.addWeighted(blend, 1, output3, alpha3, 0)
 
 # Display the images
-cv2.imshow("Original", image)
+#cv2.imshow("Original", image)
 cv2.imshow("Output 1", output1)
-cv2.imshow("Output 2", output2)
-cv2.imshow("Output 3", output3)
-cv2.imshow("Blended", blend)
+#cv2.imshow("Output 2", output2)
+#cv2.imshow("Output 3", output3)
+#cv2.imshow("Blended", blend)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
