7c00f3a479a96677015f96c888a42e768530856b
Seah <code.angz@gmail.com>
2023-03-23 14:39:46 +0900

    0323 color overlay done

--- vr2/app.py
+++ vr2/app.py
@@ -21,25 +21,64 @@ class FeatureChoice(str, enum.Enum):
 
 @app.route('/apply-makeup/', methods=['POST'])
 def try_makeup():
-    choice = request.form['choice']
+    lips = request.form['lips']
+    blush = request.form['blush']
+    foundation = request.form['foundation']
     filePath = request.form['filePath']
-    color = request.form['color']
     
-    if color=='red': color=(0,0,255)
-    elif color=='orange': color=(0,127,255)
-    elif color=='purple': color=(255,0,255)
-    elif color=='pink': color=(234,5,250)
+    # 색상별 BGR값 부여 -> lips
+    if lips=='red': color=(0,0,255)
+    elif lips=='orange': color=(0,127,255)
+    elif lips=='purple': color=(102,000,153)
+    elif lips=='lightpurple' : color=(130,0,75)
+    elif lips=='pink': color=(255,0,255)
+    elif lips=='coral' : color=(0,102,255)
+    elif lips=='beige' : color=(0,153,204)
+    elif lips=='rose' : color=(0,0,102)
+    elif lips=='darkrose' : color=(000,000,102)
+    elif lips=='salmon' : color=(000,000,204)
+    elif lips=='none' : color=()
+    
+     # 색상별 BGR값 부여 -> blush
+    if blush=='red': color=(0,0,255)
+    elif blush=='orange': color=(0,127,255)
+    elif blush=='purple': color=(102,000,153)
+    elif blush=='lightpurple' : color=(130,0,75)
+    elif blush=='pink': color=(255,0,255)
+    elif blush=='coral' : color=(0,102,255)
+    elif blush=='beige' : color=(0,153,204)
+    elif blush=='rose' : color=(0,0,102)
+    elif blush=='darkrose' : color=(000,000,102)
+    elif blush=='salmon' : color=(000,000,204)
+    elif blush=='none' : color=()
+    
+     # 색상별 BGR값 부여 -> lips
+    if foundation=='none': color=(0,0,255)
+    elif foundation=='dark': color=(0,127,255)
+    elif foundation=='warmbeige': color=(102,000,153)
+    elif foundation=='coolpink' : color=(130,0,75)
+    elif foundation=='lightbeige': color=(255,0,255)
     
     # 파일경로 불러오기
     image = cv2.imread(filePath, cv2.IMREAD_UNCHANGED)
     
-    # 메이크업 방식 선택
-    output = apply_makeup(image, False, choice, color, False)
+    # 각 부위 별 색상 넣기
+    output_lip = apply_makeup(image, False, 'lips', color, False)
+    output_blush = apply_makeup(image, False, 'blush', color, False)
+    output_foundation = apply_makeup(image, False, 'foundation', color, False)
+    
+    # Blend the three output images together
+    alpha1 = 0.5 
+    alpha2 = 0.3  
+    alpha3 = 0.2  
+    beta = 1 - (alpha1 + alpha2 + alpha3)
+    blend = cv2.addWeighted(output_lip, alpha1, output_blush, alpha2, 0)
+    blend = cv2.addWeighted(blend, 1, output_foundation, alpha3, 0)
     
     # output 저장 -> 뒤에 시간에 따른 uuid 붙음
     output_filename = f"output_{int(time.time())}_{uuid.uuid4()}.jpg"
     output_filepath = f"C:/dev64/thehyundai/color/img/{output_filename}"
-    cv2.imwrite(output_filepath, output)
+    cv2.imwrite(output_filepath, blend)
     
     # jpg로 return
     return output_filename
--- vr2/utils.py
+++ vr2/utils.py
@@ -12,20 +12,17 @@ cheeks = [425, 205]
 
 
 def apply_makeup(src: np.ndarray, is_stream: bool, feature: str, color: Tuple[int, int, int], show_landmarks: bool = False):
-    """
-    Takes in a source image and applies effects onto it.
-    """
     ret_landmarks = detect_landmarks(src, is_stream)
     height, width, _ = src.shape
     feature_landmarks = None
     if feature == 'lips':
         feature_landmarks = normalize_landmarks(ret_landmarks, height, width, upper_lip + lower_lip)
         mask = lip_mask(src, feature_landmarks, color)
-        output = cv2.addWeighted(src, 1.0, mask, 1, 0.0)
+        output = cv2.addWeighted(src, 1.0, mask, 1, 1.0)
     elif feature == 'blush':
         feature_landmarks = normalize_landmarks(ret_landmarks, height, width, cheeks)
         mask = blush_mask(src, feature_landmarks, color, 50)
-        output = cv2.addWeighted(src, 1.0, mask, 0.3, 0.0)
+        output = cv2.addWeighted(src, 1.0, mask, 1, 1.0)
     else:  # Defaults to blush for any other thing
         skin_mask = mask_skin(src)
         output = np.where(src * skin_mask >= 1, gamma_correction(src, 1.75), src)
@@ -36,19 +33,15 @@ def apply_makeup(src: np.ndarray, is_stream: bool, feature: str, color: Tuple[in
 
 def apply_feature(src: np.ndarray, feature: str, landmarks: list, color: Tuple[int, int, int], normalize: bool = False,
                   show_landmarks: bool = False):
-    """
-    Performs similar to `apply_makeup` but needs the landmarks explicitly
-    Specifically implemented to reduce the computation on the server
-    """
     height, width, _ = src.shape
     if normalize:
         landmarks = normalize_landmarks(landmarks, height, width)
     if feature == 'lips':
         mask = lip_mask(src, landmarks, color)
-        output = cv2.addWeighted(src, 1.0, mask, 0.4, 0.0)
+        output = cv2.addWeighted(src, 1.0, mask, 1.0, 1.0)
     elif feature == 'blush':
         mask = blush_mask(src, landmarks, color, 50)
-        output = cv2.addWeighted(src, 1.0, mask, 0.3, 0.0)
+        output = cv2.addWeighted(src, 1.0, mask, 1.0, 1.0)
     else:  # Does not require any landmarks for skin masking -> Foundation
         skin_mask = mask_skin(src)
         output = np.where(src * skin_mask >= 1, gamma_correction(src, 1.75), src)
@@ -58,23 +51,13 @@ def apply_feature(src: np.ndarray, feature: str, landmarks: list, color: Tuple[i
 
 
 def lip_mask(src: np.ndarray, points: np.ndarray, color: Tuple[int, int, int]):
-    """
-    Given a src image, points of lips and a desired color
-    Returns a colored mask that can be added to the src
-    """
-    mask = np.zeros_like(src)  # Create a mask
-    mask = cv2.fillPoly(mask, [points], color)  # Mask for the required facial feature
-    # Blurring the region, so it looks natural
-    # TODO: Get glossy finishes for lip colors, instead of blending in replace the region
-    mask = cv2.GaussianBlur(mask, (7, 7), 5)
+    mask = np.zeros_like(src)
+    mask = cv2.fillPoly(mask, [points], color)
+    mask = cv2.GaussianBlur(mask, (5, 5), 9)
     return mask
 
 
 def blush_mask(src: np.ndarray, points: np.ndarray, color: Tuple[int, int, int], radius: int):
-    """
-    Given a src image, points of the cheeks, desired color and radius
-    Returns a colored mask that can be added to the src
-    """
     # TODO: Make the effect more subtle
     mask = np.zeros_like(src)  # Mask that will be used for the cheeks
     for point in points:
@@ -104,19 +87,12 @@ def mask_skin(src: np.ndarray):
 
 
 def face_mask(src: np.ndarray, points: np.ndarray):
-    """
-    Given a list of face landmarks, return a closed polygon mask for the same
-    """
     mask = np.zeros_like(src)
     mask = cv2.fillPoly(mask, [points], (255, 255, 255))
     return mask
 
 
 def clicked_at(event, x, y, flags, params):
-    """
-    A useful callback that spits out the landmark index when clicked on a particular landmark
-    Note: Very sensitive to location, should be clicked exactly on the pixel
-    """
     # TODO: Add some atol to np.allclose
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f"Clicked at {x, y}")
@@ -132,9 +108,6 @@ def clicked_at(event, x, y, flags, params):
 
 
 def vignette(src: np.ndarray, sigma: int):
-    """
-    Given a src image and a sigma, returns a vignette of the src
-    """
     height, width, _ = src.shape
     kernel_x = cv2.getGaussianKernel(width, sigma)
     kernel_y = cv2.getGaussianKernel(height, sigma)
@@ -146,10 +119,6 @@ def vignette(src: np.ndarray, sigma: int):
 
 
 def face_bbox(src: np.ndarray, offset_x: int = 0, offset_y: int = 0):
-    """
-    Performs face detection on a src image, return bounding box coordinates with
-    an optional offset applied to the coordinates
-    """
     height, width, _ = src.shape
     with FaceDetection(model_selection=0) as detector:  # 0 -> dist <= 2mts from the camera
         results = detector.process(cv2.cvtColor(src, cv2.COLOR_BGR2RGB))
